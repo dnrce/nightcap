a938af2fe8837f9238965e5405b3fad3e66095fd
Dan Rice <dan@zoombody.com>
2014-04-04 13:14:14 -0400

    Implement an idle timeout and do some refactoring

--- idle.sh
+++ idle.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TIMEOUT=300
+
 pre_sleep () {
   ssh -q 192.168.0.18 "iptables -A FORWARD -p udp -d 192.168.0.32 --dport 43971 -j DROP"
 }
@@ -12,10 +14,33 @@ post_sleep () {
 NUM_USERS_LOGGED_IN=$(who -q | tail -n1 | grep -o "[0-9]*")
 NUM_ACTIVE_CONNECTIONS=$(netstat -np --inet | tail -n +3 | grep -v "127\.0\.0\.1\|transmission-d$" | wc -l)
 
-if [[ "$NUM_ACTIVE_CONNECTIONS" == "0" ]]; then
-  pre_sleep
-  /usr/sbin/pm-suspend
-  post_sleep
-else
+# utility functions to make the logic easier to read
+no_activity() {
+  [[ "$NUM_ACTIVE_CONNECTIONS" == "0" ]]
+}
+
+reset_timer() {
+  date +%s > /root/idler/timestamp
+}
+
+timed_out() {
+  current=$(date +%s)
+  last=$(cat /root/idler/timestamp)
+  [[ "$((current-last))" -gt "$TIMEOUT" ]]
+}
+
+log_connections() {
   echo "$(date) -- $NUM_ACTIVE_CONNECTIONS active connections" >> /root/idler/idler.log
+}
+
+# main program logic
+if no_activity; then
+  if timed_out; then
+    pre_sleep
+    /usr/sbin/pm-suspend
+    post_sleep
+  fi
+else
+  reset_timer
+  log_connections
 fi
