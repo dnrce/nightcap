0a4663c1be3cbac2cb8123d9309f156f68aadbf1
Dan Rice <dan@zoombody.com>
2014-04-04 20:32:23 -0400

    Start to customize Debian files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nightcap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nightcap" command="/usr/bin/nightcap"
--- debian/nightcap.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nightcap package
-#
-0 4	* * *	root	[ -x /usr/bin/nightcap_maintenance ] && /usr/bin/nightcap_maintenance
